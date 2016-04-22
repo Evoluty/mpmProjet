003ff1949a922ed7a94bc23ee853e44800ecad8b
Evoluty <franck.schmitt@hotmail.com>
2016-04-23 00:35:09 +0200

    Ajout de commentaires, modifications des constantes

--- prog.py
+++ prog.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 from math import *
-import numpy as np
+import numpy as np # allow matrix 
 import matplotlib.pyplot as plt # allow to draw graphs
+
+
+print "Program starts"
 
 
 ########################
 # Initialize constants #
 ########################
 
+print "Initialize constants"
 
 T = 1.0
-w = (2*pi)/T
-epsilon=0.001
+w = 2*pi/T
+epsilon=10**-3
 NT = 500
 dT = T/NT
 ro = 0.03
@@ -19,24 +23,26 @@ ro = 0.03
 x = np.transpose(np.atleast_2d([0.1, 0.,0.,0.]))
 u = np.transpose(np.atleast_2d([0.,0.]))
 
-A = np.mat([[0., 1., 0., 0.],
+A = np.matrix([[0., 1., 0., 0.],
               [(w**2)*3., 0., 0., 2.*w], 
               [0., 0., 0., 1.], 
               [0., -2*w, 0., 0.]])
 
-B = np.mat([[0., 0.], 
+B = np.matrix([[0., 0.], 
                [1., 0.], 
                [0., 0.], 
                [0., 1.]])
 
 
-#############################
-# Change coords for display #
-#############################
+############################
+# Change coords to display #
+############################
 
+# Returns the xCoord on circular axes
 def newCoordX(xk, yk, n):
     return xk*cos(w*n*dT) - yk*sin(w*n*dT) + cos(w*n*dT)
 
+# Returns the yCoord on circular axes
 def newCoordY(xk, yk, n):
     return xk*sin(w*n*dT) + yk*cos(w*n*dT) + sin(w*n*dT)
 
@@ -45,14 +51,16 @@ def newCoordY(xk, yk, n):
 # Solve the first equation #
 ############################
 
+# Returns xk : the solution of the current step of the euler equation
 def calculateF(xk, uk):
     return np.dot(A, xk) + np.dot(B, uk)
 
+# Returns xn : the array of the solutions of the euler equation
 def firstEquation(xk, uk):
-    xTab = [xk]
-    for i in range(0, NT):
-        valeur = calculateF(xk, uk)
-        xk = xk+dT*valeur 
+    xTab=[xk]
+    for k in range(0, NT):
+        res=calculateF(xk,uk)
+        xk = xk+dT*res
         xTab.append(xk)
     return xTab
 
@@ -61,21 +69,25 @@ def firstEquation(xk, uk):
 # Solve the second equation #
 #############################
 
+# Returns pk : the solution of the current step of the backward euler equation
 def calculateG(p):
     return -np.dot(A.T, p)
 
+# Returns pn : the array of the solutions of the backward euler equation
 def secondEquation(p):
-    pTab = [p]
-    for i in range(0, NT):
-        valeur = calculateG(p)
-        p = p - dT * valeur
+    pTab=[p]
+    for k in range(0, NT):
+        res=calculateG(p)
+        p = p-dT*res
         pTab.append(p)
     return pTab
+
 
 ############
 # Gratient #
 ############
 
+# Returns uk : the current gradient value
 def gradient(p):    
     return epsilon*u + np.dot(B.T, p)
 
@@ -84,29 +96,39 @@ def gradient(p):
 # Main #
 ########
 
+# Iterates and call the other functions
 def main():
-    k = u
+    # Uses the global u vector
+    global u
+    # Iterates and call the functions to get closer and closer to the best solution
+    print "Starts iterating"
     for i in range(0, NT):
-        xTab = firstEquation(x, k)
+        if (i%50 == 0):
+            print "Iteration number " + str(i)
+        xTab = firstEquation(x, u)
         pTab = secondEquation(xTab[NT])
         grad = gradient(pTab[NT-i])
-        k=k-ro*grad        
+        # We initialize the next u
+        u=u-ro*grad        
 
-    x1 = []
-    y1 = []
-    x2 = []
-    y2 = []
+    # Put the results in tabs and modify the values to put them in the circular base
+    print "Retrieving values" 
+    x1, y1, x2, y2 = [], [], [], []
     for i in range(0, NT):
+        # These arrays represent the mouvement of the rocket
         x1.append(newCoordX(float(xTab[i][0][0]), float(xTab[i][2][0]), i))
         y1.append(newCoordY(float(xTab[i][0][0]), float(xTab[i][2][0]), i))
-
+        # Second array is to print the circular mouvement of the ISS Station 
         x2.append(cos(w*i*dT))
         y2.append(sin(w*i*dT))
 
+    # Prints the results
+    print "Printing values"
     plt.plot(x1, y1)
     plt.plot(x2,y2)
     plt.show()
+    print "End of program"
 
-
+# Calls the main function if the code is called as a program
 if __name__=='__main__' :
     main()
